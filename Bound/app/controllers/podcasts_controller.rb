d3eb31d7d13cb077773d249e43cef568b14e38b4
Tolulope <tamenze@gmail.com>
2016-01-10 12:19:52 -0500

    Turned iTunes back on, starting to test XML parser

--- Bound/app/controllers/podcasts_controller.rb
+++ Bound/app/controllers/podcasts_controller.rb
@@ -12,27 +12,27 @@ class PodcastsController < ApplicationController
       if @genre_term.include? "_" 
         @genre_term = @genre_term.gsub("_"," ").split(" ").map(&:capitalize).join(" ")
       end
-      @podcasts = Podcast.search((@genre_term),params[:durax]).order("created_at DESC")  
+      # @podcasts = Podcast.search((@genre_term),params[:durax]).order("created_at DESC")  
       
       if (params[:durax] != nil) && (@genre_term != "commit") && @podcasts != []
-          @podcasts 
-    	else
-    	@failed_match = "No podcasts match your query :("
-      end
+     #      @podcasts 
+    	# else
+    	# @failed_match = "No podcasts match your query :("
+     #  end
 
-      # @results = ITunesSearchAPI.search(:term=> @genre_term, :country => "US", :entity=> "podcast", :media => "podcast", :limit => 10)
+      @results = ITunesSearchAPI.search(:term=> @genre_term, :country => "US", :entity=> "podcast", :media => "podcast", :limit => 10)
 
           #   r = @results.first
           # @cool = r['feedUrl']
           #  @xpage = HTTParty.get(@cool)["rss"]["channel"]
-           # @doc = Nokogiri::XML(open(@cool))
-           # @durations = @doc.xpath('//duration')
-    # # else
-    # # @failed_match = "No podcasts match your query :("
+          #  @doc = Nokogiri::XML(open(@cool))
+          #  @durations = @doc.xpath('//duration')
+    # else
+    # @failed_match = "No podcasts match your query :("
 
-    # @okayy = @results[0]
-    # @info = @results[0]["trackName"] + " " +  @results[0]["feedUrl"]
-    # end
+    @okayy = @results[0]
+    @info = @results[0]["trackName"] + " " +  @results[0]["feedUrl"]
+      end
 
   end
 
