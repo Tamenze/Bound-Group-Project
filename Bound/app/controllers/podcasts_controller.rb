9740ed908a37fdfcb6527fb3770594491594c22f
Tolulope <tamenze@gmail.com>
2016-01-11 12:44:00 -0500

    Got each podcast's duration to print along side other info, now need to make search results return based on trip time (durax variable)

--- Bound/app/controllers/podcasts_controller.rb
+++ Bound/app/controllers/podcasts_controller.rb
@@ -21,28 +21,32 @@ class PodcastsController < ApplicationController
     	# @failed_match = "No podcasts match your query :("
      #  end
 
-      @results = ITunesSearchAPI.search(:term=> @genre_term, :country => "US", :entity=> "podcast", :media => "podcast", :limit => 10)
+      @results = ITunesSearchAPI.search(:term=> @genre_term, :country => "US", :entity=> "podcast", :media => "podcast", :limit => 15)
+        @durations = []
+
+# results.each_with_index do |object, index|
+#     puts "#{object}, duration is #{durations[index]} "
+# end
+
 
         if @results
           @results.each do |x|
+
           
           @cool = x['feedUrl'] + "?format=xml"
               @doc = Nokogiri::XML(open(@cool))
-           @duration = @doc.xpath('//itunes:duration', 'itunes'=> 'http://www.itunes.com/dtds/podcast-1.0.dtd').first.content
+              
+           @durations << @doc.xpath('//itunes:duration', 'itunes'=> 'http://www.itunes.com/dtds/podcast-1.0.dtd').first.content
            # @doc.xpath('//duration').each do |duration|
 
            #write if/else that grabs dates from <pubDate> tags and gets the duration from only the most recent one(s?)
 
-            #if i change @doc to open in html, then the duration query works even though i don't see them on the page 
-          end
+            #if i change @doc to open in html, then the duration query works even though i don't see them on the page
+            end  
+        else
+          @failed_match = "No podcasts match your query :("
         end
-    # else
-    # @failed_match = "No podcasts match your query :("
-
-    @okayy = @results[0]
-    @info = @results[0]["trackName"] + " " +  @results[0]["feedUrl"]
       end
-
   end
 
 end
